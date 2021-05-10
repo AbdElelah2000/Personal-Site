fc05159115b5c07de72c0e6ee88e8fee648528b9
AbdElelah2000 <82244228+AbdElelah2000@users.noreply.github.com>
2021-05-09 20:02:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const navtoggle = document.querySelector('.nav-toggle');
+
+const navlink = document.querySelectorAll('.nav__link');
+
+navtoggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navlink.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+    
+})
